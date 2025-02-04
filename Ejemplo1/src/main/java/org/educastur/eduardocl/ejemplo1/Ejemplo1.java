744fafe60cb6ea34371c154639d7ef8cb712aba6
eduardocl <eduardocl@educastur.org>
2025-02-04 13:41:50 +0100

    Ejemplo 1 COMMIT 1

---
+++ Ejemplo1/src/main/java/org/educastur/eduardocl/ejemplo1/Ejemplo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.educastur.eduardocl.ejemplo1;
+
+/**
+ *
+ * @author eduardo
+ */
+public class Ejemplo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplo 1");
+    }
+}
